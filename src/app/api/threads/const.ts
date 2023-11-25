065582b747e4793047a8f501865a99791523c4af
Hoang Trung <hoangtrung1801.2003@gmail.com>
2023-11-25 23:31:02 +0700

    feat: update

--- src/app/api/threads/const.ts
+++ src/app/api/threads/const.ts
@@ -5,24 +5,125 @@ export const question0: ThreadMessage = {
     type: ThreadMessageType.TEXT,
     content:
         "Hello, I'm a bot. I'm here to help you sell your product. What would you like to sell?",
+    payload: {},
 };
 
 export const question1: ThreadMessage = {
     id: "question1",
     type: ThreadMessageType.QUESTION,
     content: "What your product name?",
+    payload: {},
 };
 
 export const question2: ThreadMessage = {
     id: "question2",
     type: ThreadMessageType.QUESTION,
     content: "Description of product?",
+    payload: {},
 };
 
 export const question3: ThreadMessage = {
     id: "question3",
     type: ThreadMessageType.QUESTION,
     content: "What is the price?",
+    payload: {},
+};
+
+export const question4: ThreadMessage = {
+    id: "question4",
+    type: ThreadMessageType.QUESTION,
+    content: "What style do you want for the page?",
+    payload: {},
+};
+
+export const question5: ThreadMessage = {
+    id: "question5",
+    type: ThreadMessageType.QUESTION,
+    content: "What color do you want for the page?",
+    payload: {},
+};
+
+export const questionBlock6: ThreadMessage = {
+    id: "questionblock-6",
+    type: ThreadMessageType.QUESTION,
+    content: "Choose your first block in landing page",
+    payload: {
+        blocks: [
+            {
+                id: "hero-1",
+                props: {
+                    title: "",
+                    subTitle: "",
+                },
+            },
+            {
+                id: "hero-2",
+                props: {
+                    title: "",
+                    subTitle: "",
+                },
+            },
+        ],
+    },
+};
+
+export const questionBlock7: ThreadMessage = {
+    id: "questionblock-7",
+    type: ThreadMessageType.QUESTION,
+    content: "Choose your second block in landing page",
+    payload: {
+        blocks: [
+            {
+                id: "feature-1",
+                props: {
+                    title: "",
+                    subTitle: "",
+                    description: "",
+                    features: "",
+                },
+            },
+            {
+                id: "feature-2",
+                props: {
+                    title: "",
+                    subTitle: "",
+                    description: "",
+                    features: "",
+                },
+            },
+        ],
+    },
+};
+
+export const questionBlock8: ThreadMessage = {
+    id: "questionblock-8",
+    type: ThreadMessageType.QUESTION,
+    content: "Choose your third block in landing page",
+    payload: {
+        blocks: [
+            {
+                id: "contact-1",
+                props: {
+                    title: "",
+                    subtitle: "",
+                },
+            },
+            {
+                id: "contact-2",
+                props: {
+                    title: "",
+                    subtitle: "",
+                },
+            },
+        ],
+    },
+};
+
+export const endMessage: ThreadMessage = {
+    id: "end",
+    type: ThreadMessageType.END,
+    content: null,
+    payload: {},
 };
 
 // export const question4: ThreadMessage = {
--- src/components/feature1.tsx
+++ src/components/feature1.tsx
@@ -1,56 +1,76 @@
-import {CloudArrowUpIcon, LockClosedIcon, ServerIcon} from '@heroicons/react/20/solid'
-import {ArrowPathIcon, FingerPrintIcon} from "@heroicons/react/24/outline";
+import {
+    CloudArrowUpIcon,
+    LockClosedIcon,
+    ServerIcon,
+} from "@heroicons/react/20/solid";
+import { ArrowPathIcon, FingerPrintIcon } from "@heroicons/react/24/outline";
 
 const featuresList = [
     {
-        name: 'Push to deploy.',
+        name: "Push to deploy.",
         description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
         icon: CloudArrowUpIcon,
     },
     {
-        name: 'SSL certificates.',
-        description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+        name: "SSL certificates.",
+        description:
+            "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
         icon: LockClosedIcon,
     },
     {
-        name: 'Database backups.',
-        description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+        name: "Database backups.",
+        description:
+            "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
         icon: ServerIcon,
     },
     {
-        name: 'Simple queues',
+        name: "Simple queues",
         description:
-            'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+            "Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.",
         icon: ArrowPathIcon,
     },
-]
+];
 
-export default function Feature1({title, subTitle, features = featuresList}: any) {
+export default function Feature1({
+    title,
+    subTitle,
+    description,
+    features,
+}: any) {
     return (
         <div className="overflow-hidden bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div
-                    className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                     <div className="lg:pr-8">
                         <div className="lg:max-w-lg">
-                            <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+                            <h2 className="text-base font-semibold leading-7 text-indigo-600">
+                                {subTitle}
+                            </h2>
                             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                                 {title ? title : "A better workflow"}
                             </p>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
-                                {subTitle ? subTitle : "Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione."}
+                                {description
+                                    ? description
+                                    : "Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione."}
                             </p>
                             <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                                 {features.map((feature: any) => (
-                                    <div key={feature.name} className="relative pl-9">
+                                    <div
+                                        key={feature.title}
+                                        className="relative pl-9"
+                                    >
                                         <dt className="inline font-semibold text-gray-900">
-                                            <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
-                                                          aria-hidden="true"/>
-                                            {feature.name}
-                                        </dt>
-                                        {' '}
-                                        <dd className="inline">{feature.description}</dd>
+                                            <feature.icon
+                                                className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                                                aria-hidden="true"
+                                            />
+                                            {feature.title}
+                                        </dt>{" "}
+                                        <dd className="inline">
+                                            {feature.description}
+                                        </dd>
                                     </div>
                                 ))}
                             </dl>
@@ -66,5 +86,5 @@ export default function Feature1({title, subTitle, features = featuresList}: any
                 </div>
             </div>
         </div>
-    )
+    );
 }
